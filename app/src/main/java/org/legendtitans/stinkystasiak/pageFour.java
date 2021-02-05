2a41817acb5b82cd8651923e265348023a447938
Pavan <bodapatip@s.dcsdk12.org>
2021-02-05 11:25:46 -0700

    initial commit

---
+++ app/src/main/java/org/legendtitans/stinkystasiak/pageFour.java
@@ -0,0 +1,14 @@
+package org.legendtitans.stinkystasiak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pageFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_four);
+    }
+}
